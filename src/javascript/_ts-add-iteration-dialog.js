58bb3c198e25abb304fc7369d13e231880bf9406
John Martin <curmudgeon+git@rallydev.com>
2016-06-05 17:40:49 -0700

    create iterations

--- src/javascript/_ts-add-iteration-dialog.js
+++ src/javascript/_ts-add-iteration-dialog.js
@@ -16,13 +16,34 @@ Ext.define('CA.techservices.dialog.AddIterationDialog',{
     initComponent: function() {
         this.callParent(arguments);
         
-        this.drawHeader();
-        
-        this.drawFooter();
-        
+        this._getIterationModel().then({
+            scope: this,
+            success: function(model) {
+                this.iteration_model = model;
+                this.drawHeader();
+            
+                this.drawFooter();
+            }
+        });
     },
 
+    _getIterationModel: function() {
+        var deferred = Ext.create('Deft.Deferred');
+        
+        Rally.data.ModelFactory.getModel({
+            type: 'Iteration',
+            scope: this,
+            success: function(model) {
+                model = this._changeFieldRights(model);
+                deferred.resolve(model);
+            }
+        });
+        
+        return deferred.promise;
+    },
+    
     drawHeader: function() {
+        
         this.addDocked({
             xtype:'container',
             dock:'top',
@@ -74,14 +95,26 @@ Ext.define('CA.techservices.dialog.AddIterationDialog',{
             },
             {
                 xtype:'container',
-                itemId:'task_box',
-                padding: 5,
-                tpl:'<tpl><span>To Do:</span><br/><ul>' +
-                        '<li>Create this many sprints: {numberOfSprints}</li>' +
-                        '<li>Sprint length (days): {daysInSprint}</li>' +
-                        '<li>Starting: {startDate}</li>' +
-                        '<li>Ending: {endDate}</li>' +
-                        '</ul></tpl>'
+                layout: 'hbox',
+                items: [ {
+                    xtype:'container',
+                    itemId:'task_box',
+                    padding: 5,
+                    tpl:'<tpl><span>To Do:</span><br/><ul>' +
+                            '<li>Create this many (per proj): {numberOfSprints}</li>' +
+                            '<li>Sprint length (days): {daysInSprint}</li>' +
+                            '<li>Starting: {startDate}</li>' +
+                            '<li>Ending: {endDate}</li>' +
+                            '</ul></tpl>'
+                },
+                {
+                    xtype:'textareafield',
+                    itemId:'status_box',
+                    html: '',
+                    width: 800,
+                    height: 275,
+                    padding: 10
+                }]
             }]
         });
     },
@@ -179,9 +212,7 @@ Ext.define('CA.techservices.dialog.AddIterationDialog',{
         
         start_date = Rally.util.DateTime.toIsoString(start_date);
         end_date = Rally.util.DateTime.toIsoString(end_date);
-        
-        console.log(start_date, end_date);
-        
+                
         var start_inside_filters = Rally.data.wsapi.Filter.and([
             {property:'StartDate',operator:'>=',value: start_date},
             {property:'StartDate',operator:'<=',value: end_date}
@@ -226,7 +257,7 @@ Ext.define('CA.techservices.dialog.AddIterationDialog',{
                     var noun = "iteration";
                     if ( count > 1 ) { 
                         verb = "are";
-                        nount = "iterations";
+                        noun = "iterations";
                     }
                     var message = Ext.String.format("Warning.  There {0} {1} existing overlapping {2}.",
                         verb,
@@ -234,10 +265,10 @@ Ext.define('CA.techservices.dialog.AddIterationDialog',{
                         noun
                     );
                     message_box.update({message: message});
-                    
-                    this._setConfiguration();
-                    if ( !Ext.isEmpty(button) ) { button.setDisabled(false); }
                 }
+                
+                this._setConfiguration();
+                if ( !Ext.isEmpty(button) ) { button.setDisabled(false); }
             },
             failure: function(msg) {
                 Ext.Msg.alert('Problem checking iterations', msg);
@@ -253,17 +284,206 @@ Ext.define('CA.techservices.dialog.AddIterationDialog',{
         var number_of_sprints = 1;
         
         this.copy_config = {
-                numberOfSprints: number_of_sprints,
-                daysInSprint: days_in_sprint,
-                startDate: start_date,
-                endDate: end_date
+            numberOfSprints: number_of_sprints,
+            daysInSprint: days_in_sprint,
+            startDate: start_date,
+            endDate: end_date
         };
         
         this.down('#task_box').update(this.copy_config);
     },
                     
     _create: function() {
+        var me = this;
         
+        if ( Ext.isEmpty(this.copy_config) || this.copy_config == {} ) {
+            return;
+        }
+        
+        this.setLoading('Finding projects');
+        this._getProjects(this._copyToChildProjects()).then({
+            scope: this,
+            success: function(projects) {
+                projects = Ext.Array.unique( Ext.Array.flatten(projects) );
+                
+                var number_of_sprints = this.copy_config.numberOfSprints || 1;
+                var start_date = this.copy_config.startDate;
+                var end_date = this.copy_config.endDate;
+                
+                this.setLoading(false);
+                
+                var promises = [];
+                
+                for ( var i=0; i<number_of_sprints; i++ ) {
+                    var name = Ext.Date.format(start_date, 'Y-m-d');
+                    promises.push( function() {
+                        return this._createIterations(name,start_date,end_date,projects);
+                    });
+                    
+                    start_date = Rally.util.DateTime.add(start_date, 'day', this.copy_config.daysInSprint);
+                    end_date = Rally.util.DateTime.add(end_date, 'day', this.copy_config.daysInSprint);
+                }
+                
+                Deft.Chain.sequence(promises,this).then({
+                    success: function(results) {
+                        //
+                        me._checkDates();
+                    },
+                    failure: function(msg) {
+                        Ext.Msg.alert('',msg);
+                    }
+                }).always(function(){
+                    me.setLoading(false);
+                });
+            },
+            failure: function(msg) {
+                Ext.Msg.alert("Problem fetching projects", msg);
+            }
+        });
+        
+    },
+    
+    _getProjects: function(get_children) {
+        var deferred = Ext.create('Deft.Deferred');
+                
+        var config = {
+            model:'Project',
+            filters: [{property:'ObjectID',value:Rally.getApp().getContext().getProject().ObjectID}],
+            fetch: ['Name','ObjectID','Children']
+        };
+        
+        TSUtils.loadWsapiRecords(config,false).then({
+            scope: this,
+            success: function(results) {
+                if ( !get_children || results[0].get('Children').Count === 0 ) { 
+                    deferred.resolve(results);
+                    return;
+                }
+                
+                this._getChildProjects(results[0], results).then({
+                    success: function(results) {
+                        deferred.resolve(results);
+                    },
+                    failure: function(msg) {
+                        deferred.reject(msg);
+                    }
+                });
+                
+            },
+            failure: function(msg) {
+               deferred.reject(msg);
+            }
+        });
+        
+        return deferred.promise;
+    },
+    
+    _getChildProjects: function(project, projects) {
+        var deferred = Ext.create('Deft.Deferred');
+        
+        var config = {
+            model:'Project',
+            filters: [{property:'Parent.ObjectID',value:project.get('ObjectID')}],
+            fetch: ['Name','ObjectID']
+        };
+        
+        TSUtils.loadWsapiRecords(config,false).then({
+            scope: this,
+            success: function(results) {
+                var me = this;
+                if ( results.length === 0 ) { 
+                    deferred.resolve(projects);
+                    return;
+                }
+                
+                projects = Ext.Array.push(projects, results);
+                
+                var promises = Ext.Array.map(results, function(p){
+                    return function() { 
+                        return me._getChildProjects(p,projects);
+                    };
+                });
+                
+                Deft.Chain.sequence(promises,me).then({
+                    success: function(results) {
+                        deferred.resolve(results);
+                    },
+                    failure: function(msg) {
+                        deferred.reject(msg);
+                    }
+                });
+            },
+            failure: function(msg) {
+               deferred.reject(msg);
+            }
+        });
+        
+        return deferred.promise;
+    },
+    
+    _createIterations: function(name,start_date,end_date,projects){
+        var deferred = Ext.create('Deft.Deferred'),
+            me = this;
+        
+        var promises = Ext.Array.map(projects, function(project){
+            return function() {
+                return me._createIteration(name,start_date,end_date,project);
+            }
+        });
+        
+        return Deft.Chain.sequence(promises, me);
+    },
+    
+    _createIteration: function(name,start_date,end_date,project){
+        var deferred = Ext.create('Deft.Deferred');
+        var me = this;
+        
+        this.setLoading(Ext.String.format("Creating {0} in {1}", 
+            name,
+            project.get('Name')
+        ));
+        
+        var fields = {
+            Name: name,
+            StartDate: start_date,
+            EndDate: end_date,
+            State: 'Planning',
+            Project: { _ref: project.get("_ref") }
+        };
+        
+        console.log(fields);
+        
+        var iteration = Ext.create(this.iteration_model, fields);
+        iteration.save({
+            callback: function(result, operation) {
+                if(operation.wasSuccessful()) {
+                    var text = me.down('#status_box').getValue();
+                    
+                    text = text + "\nCreated " + name + " in " + project.get('Name');
+                    
+                    me.down('#status_box').setValue(text);
+                    
+                    deferred.resolve();
+                } else {
+                    console.log(operation);
+                    deferred.reject("Problem saving iteration " + name + " (" + operation.error.errors.join(', ') + ")" );
+                }
+            }
+        });
+
+        return deferred.promise;
+    },
+    
+    _changeFieldRights: function(model) {
+        var fields = model.getFields();
+        Ext.Array.each(fields, function(field,idx) {
+            if ( field.name == "Project" ) {
+                field.readOnly = false;
+                field.persist = true;
+            }
+        });
+        
+        return model;
     }
 
     
